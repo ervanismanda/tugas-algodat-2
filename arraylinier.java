0b29f19666c3224896fd3052e93a72931bd0eba9
ervan ismanda <ervanismandaa@gmail.com>
2017-10-10 23:33:18 +0700

    tugas algodat 2

---
+++ arraylinier.java
@@ -0,0 +1,13 @@
+
+package algodat;
+
+
+public interface arraylinier {
+    public boolean isEmpety();
+    public int size();
+    public Object get(int index);
+    public int indexOf(Object element);
+    public Object remove(int index);
+    public void add(int index, Object obj);
+    public String toString();
+}
